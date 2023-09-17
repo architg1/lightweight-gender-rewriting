a3ca250ec6d3cf1d86ecfe0a85a1e721f7dbe9b6
architg1 <ag18052002@gmail.com>
2023-09-17 12:23:30 +0800

    pre-processing basics

---
+++ dataset-generation/filter_data.py
@@ -0,0 +1,19 @@
+from collections import Counter, defaultdict
+import re
+import spacy
+import matplotlib.pyplot as plt
+
+nlp = spacy.load("en_core_web_sm")
+
+MALE_PRONOUNS = ['he', 'him', 'his', 'himself']
+FEMALE_PRONOUNS = ['she', 'her', 'hers', 'herself']
+
+def is_gendered(sentence):
+    sentence = sentence.lower()
+    contains_male = any(re.search(r'\b{}\b'.format(m_pronoun), sentence) for m_pronoun in MALE_PRONOUNS)
+    contains_female = any(re.search(r'\b{}\b'.format(f_pronoun), sentence) for f_pronoun in FEMALE_PRONOUNS)
+    if contains_male and not contains_female:
+        return sentence
+    elif contains_female and not contains_male:
+        return sentence
+    return False
