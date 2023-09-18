5c3987ea6d959a56552a7a55b27466b50e42ab77
architg1 <ag18052002@gmail.com>
2023-09-18 23:55:06 +0800

    added additional code generation code

--- dataset-generation/filter_data.py
+++ dataset-generation/filter_data.py
@@ -2,6 +2,7 @@ from collections import Counter, defaultdict
 import re
 import spacy
 import matplotlib.pyplot as plt
+import pandas as pd
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -13,7 +14,33 @@ def is_gendered(sentence):
     contains_male = any(re.search(r'\b{}\b'.format(m_pronoun), sentence) for m_pronoun in MALE_PRONOUNS)
     contains_female = any(re.search(r'\b{}\b'.format(f_pronoun), sentence) for f_pronoun in FEMALE_PRONOUNS)
     if contains_male and not contains_female:
-        return sentence
+        return True
     elif contains_female and not contains_male:
-        return sentence
+        return True
     return False
+
+def process_entry(text): # for each entry
+    processed_text = []
+    doc = nlp(text)
+    sentences = list(doc.sents)
+    for sentence in sentences: # for each sentence
+        if is_gendered(sentence.text) is True: # if the sentence fulfils our criteria
+            processed_text.append(sentence.text)  # then add it back
+    return ';;'.join(processed_text) # return all sentences that fit our criteria
+
+# Load dataset
+dataset = load_dataset("wikipedia", "20220301.en")
+dataset['text (string)'] = dataset['text (string)'].apply(process_entry)
+
+"""
+gendered_dataset = []
+
+articles = dataset['text (string)']
+for article in articles:
+    sentences = nlp(article)
+    for sentence in sentences:
+        if is_gendered(sentence) is True:
+            gendered_dataset.append(sentence)
+
+gendered_dataset = pd.DataFrame({"gendered_sentence": gendered_dataset})
+"""
