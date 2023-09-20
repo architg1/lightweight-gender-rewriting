4d4aa8c0010c4adf77149a7a1fbd30020f569b06
architg1 <ag18052002@gmail.com>
2023-09-20 11:01:09 +0800

    Update filter_corpus.py
    drop all rows with >20 words

--- dataset-generation/filter_corpus.py
+++ dataset-generation/filter_corpus.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import numpy as np
 
+def count_words(text):
+    words = text.split()
+    return len(words)
+
 # Load original corpus
 df = pd.read_csv('/Users/architg/Documents/GitHub/final-year-project/data/wikipedia_corpus.csv')
 
-df = df.dropna()
+df = df.dropna() # drop empty rows
+df = df[df['biased'].apply(lambda x: count_words(x) <= 20)] # drop sentences with >20 words
+df.reset_index(drop=True, inplace=True) # reset index
 
 # Save filtered corpus
 df.to_csv('/Users/architg/Documents/GitHub/final-year-project/data/wikipedia_corpus_filtered.csv', index=False)
