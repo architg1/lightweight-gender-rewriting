7f5ae2e66bb597aeb8b9b284ab455f04cf65059b
architg1 <ag18052002@gmail.com>
2023-09-28 16:39:39 +0800

    Update filter_corpus.py

--- dataset-generation/filter_corpus.py
+++ dataset-generation/filter_corpus.py
@@ -17,6 +17,7 @@ df_reverse = df_reverse.dropna() # drop empty rows
 df_reverse = df_reverse[df_reverse['biased'].apply(lambda x: count_words(x) <= 20)] # drop sentences with >20 words
 df_reverse.reset_index(drop=True, inplace=True) # reset index
 
+print(len(df), len(df_reverse))
 # Save filtered corpus
 df.to_csv('/Users/architg/Documents/GitHub/final-year-project/data/wikipedia_corpus_filtered.csv', index=False)
 df_reverse.to_csv('/Users/architg/Documents/GitHub/final-year-project/data/wikipedia_corpus_reverse_filtered.csv', index=False)
